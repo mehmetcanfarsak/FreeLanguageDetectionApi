1ddef8cd7ff09afb3b25a0a1b4d3738ff691d9ca
Mehmet Can Farsak <32980057+mehmetcanfarsak@users.noreply.github.com>
2024-04-14 13:56:10 +0300

    format document and small typo fix in language list

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ class LanguageResult(BaseModel):
 
 
 class TextToDetectModel(BaseModel):
-    text_to_detect_language: str = Field(description="For high accuracy please write at least 3 words",default="how are you")
+    text_to_detect_language: str = Field(
+        description="For high accuracy please write at least 3 words",
+        default="how are you",
+    )
 
 
 @app.get("/", include_in_schema=False, response_class=HTMLResponse)
@@ -31,34 +34,113 @@ def root():
     return markdown2.markdown(readme_content)
 
 
-@app.get("/detect_language", response_model=LanguageResult,
-         description="Returns probabilties of possible languages and most probable language")
+@app.get(
+    "/detect_language",
+    response_model=LanguageResult,
+    description="Returns probabilties of possible languages and most probable language",
+)
 def detect_language_from_query(
-        text_to_detect_language: str = Query(description="For high accuracy please write at least 3 words",default="how are you")):
+    text_to_detect_language: str = Query(
+        description="For high accuracy please write at least 3 words",
+        default="how are you",
+    )
+):
     detection_result = detect_langs(text_to_detect_language)
-    if (len(detection_result) > 0):
+    if len(detection_result) > 0:
         probable_languages = []
         for probable_language in detection_result:
-            probable_languages.append({"lang": probable_language.lang, "prob": probable_language.prob})
-        return {"most_probable_language": detection_result[0].lang, "probable_languages": probable_languages,"text":text_to_detect_language}
+            probable_languages.append(
+                {"lang": probable_language.lang, "prob": probable_language.prob}
+            )
+        return {
+            "most_probable_language": detection_result[0].lang,
+            "probable_languages": probable_languages,
+            "text": text_to_detect_language,
+        }
     return LanguageResult()
 
 
-@app.post("/detect_language", response_model=LanguageResult,
-          description="Returns probabilties of possible languages and most probable language. For high accuracy please write at least 3 words")
+@app.post(
+    "/detect_language",
+    response_model=LanguageResult,
+    description="Returns probabilties of possible languages and most probable language. For high accuracy please write at least 3 words",
+)
 def detect_language_from_query(texttodetect: TextToDetectModel):
     detection_result = detect_langs(texttodetect.text_to_detect_language)
-    if (len(detection_result) > 0):
+    if len(detection_result) > 0:
         probable_languages = []
         for probable_language in detection_result:
-            probable_languages.append({"lang": probable_language.lang, "prob": probable_language.prob})
-        return {"most_probable_language": detection_result[0].lang, "probable_languages": probable_languages,"text":texttodetect.text_to_detect_language}
+            probable_languages.append(
+                {"lang": probable_language.lang, "prob": probable_language.prob}
+            )
+        return {
+            "most_probable_language": detection_result[0].lang,
+            "probable_languages": probable_languages,
+            "text": texttodetect.text_to_detect_language,
+        }
     return LanguageResult()
 
 
-@app.get("/get_list_of_languages", response_class=JSONResponse, description="Get all available languages list.")
+@app.get(
+    "/get_list_of_languages",
+    response_class=JSONResponse,
+    description="Get all available languages list.",
+)
 def get_list_of_languages():
-    return ["af", "ar", "bg", "bn", "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "fa", "fi, fr, gu, he",
-            "hi", "hr", "hu", "id", "it", "ja", "kn", "ko", "lt", "lv", "mk", "ml", "mr", "ne", "nl", "no", "pa", "pl",
-            "pt", "ro", "ru", "sk", "sl", "so", "sq", "sv", "sw", "ta", "te", "th", "tl", "tr", "uk", "ur", "vi",
-            "zh-cn", "zh-tw"]
+    return [
+        "af",
+        "ar",
+        "bg",
+        "bn",
+        "ca",
+        "cs",
+        "cy",
+        "da",
+        "de",
+        "el",
+        "en",
+        "es",
+        "et",
+        "fa",
+        "fi",
+        "fr",
+        "gu",
+        "he",
+        "hi",
+        "hr",
+        "hu",
+        "id",
+        "it",
+        "ja",
+        "kn",
+        "ko",
+        "lt",
+        "lv",
+        "mk",
+        "ml",
+        "mr",
+        "ne",
+        "nl",
+        "no",
+        "pa",
+        "pl",
+        "pt",
+        "ro",
+        "ru",
+        "sk",
+        "sl",
+        "so",
+        "sq",
+        "sv",
+        "sw",
+        "ta",
+        "te",
+        "th",
+        "tl",
+        "tr",
+        "uk",
+        "ur",
+        "vi",
+        "zh-cn",
+        "zh-tw",
+    ]
